4b0e23d47435062fa2339138732d8ba1dc3fc8b5
James MA <super169@126.com>
2016-12-29 03:15:48 +0800

    Try to apply suggestions from code analyzer

--- app/src/main/java/org/super169/mylocation/SmsMessageReceiver.java
+++ app/src/main/java/org/super169/mylocation/SmsMessageReceiver.java
@@ -1,12 +1,17 @@
 package org.super169.mylocation;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -23,29 +28,32 @@ import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
-import org.super169.mylocation.R;
-
 /**
  * Created by James on 10/12/2014.
+ * SmsMessageReceiver : customized sms receiver, check SMS content to find out request from WhereAreYou
  */
 public class SmsMessageReceiver extends BroadcastReceiver implements
         ConnectionCallbacks, OnConnectionFailedListener {
     /** Tag string for our debug logs */
     private static final String TAG = "SmsMessageReceiver";
-    public static final String ACTION_SMS_SENT = "org.super169.mylocation.SMS_SENT_ACTION";
+    private static final String ACTION_SMS_SENT = "org.super169.mylocation.SMS_SENT_ACTION";
 
     private static GPSTracker gps;
     private static Location mLocationGps;
     private static Location mLocationNetwork;
     private static Location mLocationFused;
 
-    private static DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    // It is intended to use a fixed date format.
+    @SuppressLint("SimpleDateFormat")
+    private static final DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     /**
      * Provides the entry point to Google Play services.
      */
     private static GoogleApiClient mGoogleApiClient;
 
+    // TODO: try to avoid putting Context in static variable in later version
+    @SuppressLint("StaticFieldLeak")
     private static Context mContext;
     private static String mRecipient;
     private static String mReqParameters;
@@ -66,21 +74,22 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
         if (extras == null)
             return;
 
-        Object[] pdus = (Object[]) extras.get("pdus");
         String keyword, dataKeyword, debugKeyword;
         keyword = getPrefData(context, context.getString(R.string.pref_key_keyword), context.getString(R.string.pref_keyword_default)).toLowerCase();
         dataKeyword = '#' + keyword;
         debugKeyword = '*' + keyword;
 
-        SmsMessage[] msgs = msgs = android.provider.Telephony.Sms.Intents.getMessagesFromIntent(intent);
-        for (int i = 0; i < msgs.length; i++) {
-            SmsMessage message = msgs[i];
+        SmsMessage[] msgs = android.provider.Telephony.Sms.Intents.getMessagesFromIntent(intent);
+        for (SmsMessage message : msgs) {
             String fromAddress = message.getOriginatingAddress();
-            String msgBody = message.getMessageBody().toString().toLowerCase();
+            String msgBody = message.getMessageBody().toLowerCase();
 
-            if (msgBody.equals(keyword) || msgBody.startsWith(keyword+":")) mReqDataType = reqDataType.URL;
-            else if (msgBody.equals(dataKeyword) || msgBody.startsWith(dataKeyword+":")) mReqDataType = reqDataType.DATA;
-            else if (msgBody.equals(debugKeyword) || msgBody.startsWith(debugKeyword+":")) mReqDataType = reqDataType.DEBUG;
+            if (msgBody.equals(keyword) || msgBody.startsWith(keyword + ":"))
+                mReqDataType = reqDataType.URL;
+            else if (msgBody.equals(dataKeyword) || msgBody.startsWith(dataKeyword + ":"))
+                mReqDataType = reqDataType.DATA;
+            else if (msgBody.equals(debugKeyword) || msgBody.startsWith(debugKeyword + ":"))
+                mReqDataType = reqDataType.DEBUG;
             else mReqDataType = reqDataType.OTHERS;
 
             if (mReqDataType != reqDataType.OTHERS) {
@@ -110,36 +119,40 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
         }
     }
 
-    private boolean getGpsLocation(Context context) {
-        if (gps == null) gps = new GPSTracker(context);
-        mLocationGps = gps.getLocationGps();
-        return (mLocationGps != null);
+    private void getGpsLocation(Context context) {
+        if (gps == null) gps = new GPSTracker();
+        mLocationGps = gps.getLocationGps(context);
+        // return (mLocationGps != null);
     }
 
-    private boolean getNetowrkLocation(Context context) {
-        if (gps == null) gps = new GPSTracker(context);
-        mLocationNetwork = gps.getLocationNetwork();
-        return (mLocationNetwork != null);
+    private void getNetworkLocation(Context context) {
+        if (gps == null) gps = new GPSTracker();
+        mLocationNetwork = gps.getLocationNetwork(context);
+        // return (mLocationNetwork != null);
     }
 
     private void prepareLocation(Context context, String recipient) {
 
         getGpsLocation(context);
 
-        getNetowrkLocation(context);
+        getNetworkLocation(context);
 
         if (mGoogleApiClient == null) {
+
             mGoogleApiClient = new GoogleApiClient.Builder(context)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
+
         }
 
+        // Will it return null from GoogleApiClient builder?
+        //noinspection ConstantConditions
         if (mGoogleApiClient == null) {
             Log.d(TAG, "GoogleApiClient not ready");
             sendLocation(context, recipient);
-        }  else {
+        } else {
             mContext = context;
             mRecipient = recipient;
             Log.d(TAG, "Wait for fusedLocation");
@@ -153,17 +166,14 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
                 DATA:       #location#%s#%s;%s;%f;%f;%f;%f#
                 DEBUG:      #%s#%s;%s;%f;%f;%.2fm;%.2fm#
         */
-
-
     private void sendLocation(Context context, String recipient) {
 
         String msgContent = "";
         SmsManager sms = SmsManager.getDefault();
 
 
-        String msgFormatter = "";
+        String msgFormatter;
         String dataVersion = context.getString(R.string.app_data_version);
-        Boolean useDefault = true;
         switch (mReqDataType) {
             case URL:
                 msgFormatter = ": %s: %s: http://maps.google.com/maps?q=%f+%f  {A: %.2fm; S:%.2fm}";
@@ -186,7 +196,7 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
         }
     }
 
-    private String getLocationReturn(String formatter,  boolean singleOutput) {
+    private String getLocationReturn(String formatter, boolean singleOutput) {
         Boolean useDefault = true;
         String returnString = "";
 
@@ -208,9 +218,9 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
             if (singleOutput) return returnString;
         }
 
-        if (useDefault)  {
+        if (useDefault) {
             Location mLocationDefault = mLocationFused;
-            // In very rare case, fued locaiton is not available
+            // In very rare case, fused location is not available
             if (mLocationDefault == null) {
                 if ((mLocationGps == null) || ((mLocationNetwork != null) && (mLocationNetwork.getTime() > mLocationGps.getTime()))) {
                     mLocationDefault = mLocationNetwork;
@@ -223,19 +233,21 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
         return returnString;
     }
 
-    public String getLocationString(String formatter, Location location) {
+    private String getLocationString(String formatter, Location location) {
         if (location == null) return "";
         return String.format(formatter, location.getProvider(), format.format(new Date(location.getTime())), location.getLatitude(), location.getLongitude(), location.getAccuracy(), location.getSpeed());
     }
 
 
-    private void setPrefData(Context context, String key, String value) {
-        SharedPreferences sharedPref = context.getSharedPreferences(
-                context.getString(R.string.shared_pref_key), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(key, value);
-        editor.commit();
-    }
+// --Commented out by Inspection START (29/12/2016 1:04 AM):
+//    private void setPrefData(Context context, String key, String value) {
+//        SharedPreferences sharedPref = context.getSharedPreferences(
+//                context.getString(R.string.shared_pref_key), Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPref.edit();
+//        editor.putString(key, value);
+//        editor.apply();
+//    }
+// --Commented out by Inspection STOP (29/12/2016 1:04 AM)
 
     private String getPrefData(Context context, String key, String defValue) {
         SharedPreferences sharedPref = context.getSharedPreferences(
@@ -246,7 +258,14 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
     @Override
     public void onConnected(Bundle bundle) {
         Log.d(TAG, "GoogleApiClient connected");
-        mLocationFused = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mContext != null) {
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                mLocationFused = null;
+            } else {
+                mLocationFused = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            }
+        }
         resumeSendLocation();
     }
 
@@ -258,7 +277,7 @@ public class SmsMessageReceiver extends BroadcastReceiver implements
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.d(TAG, "GoogleApiClient connection failed");
         mLocationFused = null;
         resumeSendLocation();
